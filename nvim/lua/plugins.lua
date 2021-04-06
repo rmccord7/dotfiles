c5c033067771af1655dde233c57870de18d2fd46
Ryan McCord <rmccord@qti.qualcomm.com>
2021-04-06 12:36:33 -0400

    -Updated Packer from opt/ to start. -Updated Linux conf to install neovim. -Install script is now executable.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,11 +4,10 @@ vim.cmd('autocmd BufWritePost plugins.lua PackerCompile')
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
   execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
 end
 
 return require('packer').startup(function()
