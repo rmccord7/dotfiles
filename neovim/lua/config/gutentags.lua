9193efe709e4717f5ef61954aa8e151d5e6b37a9
Ryan McCord <ryan.mccord07@gmail.com>
2021-06-12 10:48:16 -0400

    Added gutentags config. Updated mappings.

---
+++ neovim/lua/config/gutentags.lua
@@ -0,0 +1,5 @@
+-- config project root markers.
+vim.g.gutentags_project_root = {'.p4config', 'compile_commands.json', '.git'}
+
+-- Do not generate if tag file is missing
+vim.g.gutentags_generate_on_missing = 0;
