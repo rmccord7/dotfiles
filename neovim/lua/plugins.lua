a0bc1896a6ce1b9bed2057036375fcb79bbc5f79
Ryan McCord <ryan.mccord07@gmail.com>
2021-11-28 09:24:48 -0500

    Added TODO comment plugin

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -145,6 +145,15 @@ return require('packer').startup(function(use)
     end
   }
 
+  use {
+  "folke/todo-comments.nvim",
+  requires = "nvim-lua/plenary.nvim",
+  config = function()
+    require("todo-comments").setup {
+    }
+  end
+  }
+
   -- Fuzzy finder
   use {
     'nvim-telescope/telescope.nvim',
