668d4b541f7ab6ecb877762469ac3d8ccb4e7826
Ryan McCord <ryan.mccord07@gmail.com>
2023-06-18 10:50:39 -0400

    -Updated mac os install.

--- install/install_mac.sh
+++ install/install_mac.sh
@@ -3,13 +3,18 @@
 set -e
 
 CONFIG="yaml/install.mac.conf.yaml"
-DOTBOT_DIR="dotbot"
 
-DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DOTBOT_DIR="../dotbot"
+DOTBOT_BIN="bin/dotbot"
+
+#echo "BASH_SOURCE: ${BASH_SOURCE[0]}"
+#echo "BASEDIR: ${BASEDIR}"
+#echo "DOTBOT_DIR: ${DOTBOT_DIR}"
 
 cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
-"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}/.." -c "${CONFIG}" "${@}"
