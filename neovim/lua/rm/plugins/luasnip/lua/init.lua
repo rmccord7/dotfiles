379c6156fc3b8e3e540448e1653cbe2a4970b3f4
Ryan McCord <ryan.mccord07@gmail.com>
2024-02-24 12:38:48 -0500

    fix(neovim): luasnip path

--- neovim/lua/rm/plugins/luasnip/lua/init.lua
+++ neovim/lua/rm/plugins/luasnip/lua/init.lua
@@ -4,6 +4,9 @@ if not ok then
     return
 end
 
+local config = require('rm.config')
+local path_util = require('rm.util.path')
+
 ls.config.set_config({
     -- Allows reentering old snippets
     history = true,
@@ -21,6 +24,6 @@ ls.config.set_config({
     ft_func = require('luasnip.extras.filetype_functions').from_pos_or_filetype,
 })
 
--- Set up actual snippets, TODO: lazy loading
-require('luasnip.loaders.from_vscode').lazy_load({ paths = { '~/dotfiles/neovim/lua/plugins/luasnip/vscode' } })
+-- Set up actual snippets
+require('luasnip.loaders.from_vscode').lazy_load({ paths = { config.path.plugins .. path_util.os_path('/luasnip/vscode') } })
 require('luasnip.loaders.from_lua').load()
