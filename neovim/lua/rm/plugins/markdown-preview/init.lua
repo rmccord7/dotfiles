df023666dfb0785c5908875e76cf7e4f57ab353b
Ryan McCord <ryan.mccord07@gmail.com>
2024-05-27 10:55:37 -0400

    fix: add markdown preview plugin

---
+++ neovim/lua/rm/plugins/markdown-preview/init.lua
@@ -0,0 +1,21 @@
+local config = function()
+    local ok, _ = pcall(require, 'markdown-preview')
+
+    if not ok then
+        return
+    end
+
+    vim.g.mkdp_auto_start = 0
+end
+
+local M = {
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = "cd app && npm install",
+        config = config,
+    },
+}
+
+return M
