20da3251ff111f3c0081f99c078df4719295ccc5
Ryan McCord <ryan.mccord07@gmail.com>
2024-08-03 16:43:40 -0400

    chore (neovim): removed reach plugin

--- neovim/lua/rm/plugins/init.lua
+++ neovim/lua/rm/plugins/init.lua
@@ -19,7 +19,6 @@ return {
     require('rm.plugins.nvim-neoclip'),
     require('rm.plugins.nvim-surround'),
     require('rm.plugins.p4'),
-    require('rm.plugins.reach'),
     require('rm.plugins.ss1pwn'),
     require('rm.plugins.telescope'),
     require('rm.plugins.todo-comments'),
