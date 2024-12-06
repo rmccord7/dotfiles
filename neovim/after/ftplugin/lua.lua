41bbf6ba25ac2711d96a4ba24fc4bb333a92eb06
Ryan McCord <ryan.mccord07@gmail.com>
2024-12-06 09:41:07 -0500

    feat: lua linting

--- neovim/after/ftplugin/lua.lua
+++ neovim/after/ftplugin/lua.lua
@@ -1,3 +1,5 @@
+local lint = require("lint")
+
 local bufnr = vim.api.nvim_get_current_buf()
 
 if vim.b[bufnr].rm_did_ftplugin then
@@ -13,4 +15,18 @@ vim.bo.shiftwidth = 4 -- Size of an indent
 vim.bo.smartindent = true -- Insert indents automatically
 vim.bo.softtabstop = 4 -- Number of spaces tabs count for
 vim.bo.tabstop = 4 -- Number of spaces in a tab
+
+lint.linters_by_ft = {
+  lua = {'selene'},
+}
+
+local ns = lint.get_namespace('selene')
+vim.diagnostic.config(
+    vim.tbl_deep_extend('force', vim.diagnostic.config(), {
+        virtual_text = {
+            suffix = ' ⁅selene⁆',
+        },
+    }),
+    ns
+)
 
---
+++ neovim/lua/rm/plugins/nvim-lint/init.lua
@@ -0,0 +1,35 @@
+local config = function()
+    local ok, lint = pcall(require, 'lint')
+
+    if not ok then
+        return
+    end
+
+    local api = vim.api
+
+    -- Linting –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    local lint_group = api.nvim_create_augroup('lint', { clear = true })
+    api.nvim_create_autocmd({
+        'FileReadPost',
+        'InsertLeave',
+        'BufWritePost',
+        'TextChanged',
+        'ModeChanged',
+        'FocusGained',
+    }, {
+        group = lint_group,
+        callback = function()
+            lint.try_lint()
+        end,
+    })
+end
+
+local M = {
+    {
+        'mfussenegger/nvim-lint',
+        config = config,
+    },
+
+}
+
+return M
