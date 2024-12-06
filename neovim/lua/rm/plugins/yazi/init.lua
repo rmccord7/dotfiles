403deffb8955effec98ed94485df5c0a69a6ae86
Ryan McCord <ryan.mccord07@gmail.com>
2024-12-06 09:41:52 -0500

    chore: update yazi config

--- neovim/lua/rm/plugins/yazi/init.lua
+++ neovim/lua/rm/plugins/yazi/init.lua
@@ -33,7 +33,10 @@ local M = {
         ---@type YaziConfig
         opts = {
             -- if you want to open yazi instead of netrw, see below for more info
-            open_for_directories = true,
+            open_for_directories = false,
+            keymaps = {
+              show_help = '<f1>',
+            },
         },
     },
 }
