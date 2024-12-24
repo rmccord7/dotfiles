6fcf27f99d6783bbf0459fbc6a41a05002692269
Ryan McCord <ryan.mccord07@gmail.com>
2024-12-24 14:31:55 -0500

    refactor(neovim): default lua line spacing

--- neovim/after/ftplugin/lua.lua
+++ neovim/after/ftplugin/lua.lua
@@ -11,10 +11,10 @@ vim.b[bufnr].rm_did_ftplugin = true
 -- Buffer options
 vim.bo.autoindent = true -- Insert indents automatically
 vim.bo.expandtab = true -- Expands tabs to spaces
-vim.bo.shiftwidth = 4 -- Size of an indent
+vim.bo.shiftwidth = 2 -- Size of an indent
 vim.bo.smartindent = true -- Insert indents automatically
-vim.bo.softtabstop = 4 -- Number of spaces tabs count for
-vim.bo.tabstop = 4 -- Number of spaces in a tab
+vim.bo.softtabstop = 2 -- Number of spaces tabs count for
+vim.bo.tabstop = 2 -- Number of spaces in a tab
 
 lint.linters_by_ft = {
   lua = {'selene'},
