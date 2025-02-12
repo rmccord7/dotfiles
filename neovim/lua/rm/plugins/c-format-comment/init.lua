30102809018cef16ac79810004912d9507212b27
Ryan McCord <ryan.mccord07@gmail.com>
2025-02-11 21:04:37 -0500

    chore(neovim): update c-format-comment keybind

--- neovim/lua/rm/plugins/c-format-comment/init.lua
+++ neovim/lua/rm/plugins/c-format-comment/init.lua
@@ -4,9 +4,9 @@ return {
   dev = true,
   config = function(_, _)
     vim.keymap.set(
-      { 'v' },
+      { 'n', 'v' },
       '<leader>gq',
-      [[:lua require("c-format-comment").format_all()<CR>]],
+      [[:lua require("c-format-comment").format()<CR>]],
       { nowait = true, desc = "Format all C comments in the visual selection" }
     )
   end,
