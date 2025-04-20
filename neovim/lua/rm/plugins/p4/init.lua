c252ebd53539156f391d9ba5a5a2ecfdda53fd34
Ryan McCord <ryan.mccord07@gmail.com>
2025-04-20 18:14:38 -0400

    chore(neovim): update p4 keymaps

--- neovim/lua/rm/plugins/p4/init.lua
+++ neovim/lua/rm/plugins/p4/init.lua
@@ -3,20 +3,25 @@ return {
   dependencies = {
     'nvim-neotest/nvim-nio',
     'nvim-telescope/telescope.nvim',
+    {
+      "ColinKennedy/mega.cmdparse",
+      dependencies = { "ColinKennedy/mega.logging" },
+      version = "v1.*",
+    },
   },
   dev = true,
   config = function(_, _)
     vim.keymap.set(
       { 'n' },
       '<leader>pc',
-      [[:P4 Display_CLs<CR>]],
+      [[:P4 Clients display<CR>]],
       { nowait = true, desc = "Open picker for the current client's CLs" }
     )
 
     vim.keymap.set(
       { 'n' },
       '<leader>po',
-      [[:P4 Display_Open_Files<CR>]],
+      [[:P4 opened<CR>]],
       { nowait = true, desc = "Open picker for the current client's open files" }
     )
   end,
